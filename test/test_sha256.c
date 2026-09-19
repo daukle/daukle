4c5a52f0d2278bc20a3bf5008c4af25595c59f42
intisy (Finn Birich) <finn@birich.de>
2026-09-19 12:54:28 +0200

    test(sha256): pin the padding boundary lengths

--- test/test_sha256.c
+++ test/test_sha256.c
@@ -28,6 +28,29 @@ TEST hashes_a_multi_block_input(void) {
     PASS();
 }
 
+TEST hashes_the_padding_boundary_lengths(void) {
+    char data[64];
+    char hex[65];
+
+    memset(data, 'a', 55);
+    fr_sha256_hex(data, 55, hex);
+    ASSERT_STR_EQ("9f4390f8d30c2dd92ec9f095b65e2b9ae9b0a925a5258e241c9f1e910f734318", hex);
+
+    memset(data, 'a', 56);
+    fr_sha256_hex(data, 56, hex);
+    ASSERT_STR_EQ("b35439a4ac6f0948b6d6f9e3c6af0f5f590ce20f1bde7090ef7970686ec6738a", hex);
+
+    memset(data, 'a', 63);
+    fr_sha256_hex(data, 63, hex);
+    ASSERT_STR_EQ("7d3e74a05d7db15bce4ad9ec0658ea98e3f06eeecf16b4c6fff2da457ddc2f34", hex);
+
+    memset(data, 'a', 64);
+    fr_sha256_hex(data, sizeof data, hex);
+    ASSERT_STR_EQ("ffe054fe7ae0cb6dc65c3af9b61d5209f439851db43d0ba5997337df154668eb", hex);
+
+    PASS();
+}
+
 GREATEST_MAIN_DEFS();
 
 int main(int argc, char **argv) {
@@ -35,5 +58,6 @@ int main(int argc, char **argv) {
     RUN_TEST(hashes_the_empty_input);
     RUN_TEST(hashes_abc);
     RUN_TEST(hashes_a_multi_block_input);
+    RUN_TEST(hashes_the_padding_boundary_lengths);
     GREATEST_MAIN_END();
 }
